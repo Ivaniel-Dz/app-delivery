669c3de97a38e40e311735a2d3ded1f160a99ae2
Ivaniel-Dz <pcivaniel@gmail.com>
2025-02-22 20:14:18 -0500

    add: carrito service

--- src/app/components/contador-cantidad/contador-cantidad.component.ts
+++ src/app/components/contador-cantidad/contador-cantidad.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output, signal } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, signal } from '@angular/core';
 
 @Component({
   selector: 'app-contador-cantidad',
@@ -6,9 +6,14 @@ import { Component, EventEmitter, Output, signal } from '@angular/core';
   templateUrl: './contador-cantidad.component.html',
   styleUrl: './contador-cantidad.component.scss',
 })
-export class ContadorCantidadComponent {
+export class ContadorCantidadComponent implements OnInit {
   numero = signal(1);
   @Output() cantidadCambiada = new EventEmitter<number>();
+  @Input() cantidadInicial = 1;
+
+  ngOnInit(): void {
+    this.numero.set(this.cantidadInicial);
+  }
 
   actualizarNumero(diferencia: number) {
     this.numero.set(Math.max(this.numero() + diferencia, 1));
--- src/app/pages/articulo/articulo.component.ts
+++ src/app/pages/articulo/articulo.component.ts
@@ -1,39 +1,55 @@
 import { Component, inject, signal } from '@angular/core';
 import { HeaderService } from '../../services/header.service';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { ProductosService } from '../../services/productos.service';
 import { Producto } from '../../interfaces/productos';
 import { CommonModule } from '@angular/common';
 import { ContadorCantidadComponent } from '../../components/contador-cantidad/contador-cantidad.component';
+import { CartService } from '../../services/cart.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-articulo',
-  imports: [CommonModule, ContadorCantidadComponent],
+  imports: [CommonModule, ContadorCantidadComponent,FormsModule],
   templateUrl: './articulo.component.html',
-  styleUrl: './articulo.component.scss'
+  styleUrl: './articulo.component.scss',
 })
 export class ArticuloComponent {
   //Infectamos el servicio de header a este componente
   headerService = inject(HeaderService);
   productosService = inject(ProductosService);
+  cartService = inject(CartService);
   // variable para guardar los productos
-  producto?:Producto;
+  producto?: Producto;
   cantidad = signal(1);
+  notas: string = '';
+
   //Cada que carga que cambie el valor de titulo
   ngOnInit(): void {
     this.headerService.titulo.set('Articulo');
   }
 
   //obtiene el producto por el id
-  constructor( private ac:ActivatedRoute ){
-    ac.params.subscribe(param => {
-      if(param['id']){
-        this.productosService.getById(param['id']).then(producto => {
+  constructor(private ac: ActivatedRoute, private router: Router) {
+    ac.params.subscribe((param) => {
+      if (param['id']) {
+        this.productosService.getById(param['id']).then((producto) => {
           this.producto = producto;
           this.headerService.titulo.set(producto!.nombre);
-        })
+        });
       }
-    })
+    });
   }
 
+  //Método para agregar producto al carrito
+  agregarAlCarrito() {
+    if (this.producto) {
+      this.cartService.agregarProducto(
+        this.producto?.id,
+        this.cantidad(),
+        this.notas
+      );
+      this.router.navigate(['/carrito']);
+    }
+  }
 }
