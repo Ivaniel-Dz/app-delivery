b0fb16c2747510ea53d27e5c67d5916315041429
Ivaniel-Dz <pcivaniel@gmail.com>
2025-02-22 21:52:54 -0500

    add: page carrtito

--- src/app/pages/carrito/carrito.component.ts
+++ src/app/pages/carrito/carrito.component.ts
@@ -3,25 +3,58 @@ import { HeaderService } from '../../services/header.service';
 import { CartService } from '../../services/cart.service';
 import { CommonModule } from '@angular/common';
 import { ContadorCantidadComponent } from '../../components/contador-cantidad/contador-cantidad.component';
+import { Producto } from '../../interfaces/productos';
+import { ProductosService } from '../../services/productos.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-carrito',
-  imports: [CommonModule,ContadorCantidadComponent],
+  imports: [CommonModule, ContadorCantidadComponent, RouterModule],
   templateUrl: './carrito.component.html',
-  styleUrl: './carrito.component.scss'
+  styleUrl: './carrito.component.scss',
 })
 export class CarritoComponent {
   //Infectamos el servicio de header a este componente
   headerService = inject(HeaderService);
-  cartService = inject(CartService)
+  cartService = inject(CartService);
+  productosService = inject(ProductosService);
 
-  //Cada que carga que cambie el valor de titulo
+  //variable para guardar los productos del carrito
+  productosCarrito: Producto[] = [];
+
+  subtotal: number = 0;
+  delivery: number = 100;
+  total: number = 0;
+
+  //Cada vez que cargue
   ngOnInit(): void {
+    //Cambia el titulo
     this.headerService.titulo.set('Carrito');
+    //
+    this.cartService.carrito.forEach(async (itemCarrito) => {
+      const res = await this.productosService.getById(itemCarrito.idProducto);
+      if (res) this.productosCarrito.push(res);
+      this.calcularInformacion();
+    });
   }
 
-  eliminarProducto(idProducto:number){
-    this.cartService.eliminarProducto(idProducto)
+  eliminarProducto(idProducto: number) {
+    this.cartService.eliminarProducto(idProducto);
   }
 
+  calcularInformacion() {
+    this.subtotal = 0;
+    for (let i = 0; i < this.cartService.carrito.length; i++) {
+      this.subtotal +=
+        this.productosCarrito[i].precio * this.cartService.carrito[i].cantidad;
+    }
+
+    this.total = this.subtotal + this.delivery;
+  }
+
+  //Actualiza el total de precio al agregar mas producto
+  cambiarCantidadProducto(id:number, cantidad:number){
+    this.cartService.cambiarCantidadProducto(id, cantidad)
+    this.calcularInformacion();
+  }
 }
